90602cdd6caf14582fd9cb29bde48cdf14646e0a
Tzu-Yee Yang <fdja1974@gmail.com>
2021-03-16 23:58:45 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -69,12 +69,13 @@ dconf write /org/gnome/desktop/session/idle-delay "uint32 900"
 
 step "Get oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
-git clone https://github.com/zsh-users/zsh-autosuggestions.git ${HOME}/.oh-my-zsh/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${HOME}/.oh-my-zsh/plugins/zsh-syntax-highlighting
+git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git ${HOME}/.oh-my-zsh/plugins/zsh-autosuggestions
+git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ${HOME}/.oh-my-zsh/plugins/zsh-syntax-highlighting
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone --depth=1 https://github.com/MichaelAquilina/zsh-you-should-use.git $ZSH_CUSTOM/plugins/you-should-use
 
 step "Configure tmux"
-git clone https://github.com/gpakosz/.tmux.git ${HOME}/.tmux
+git clone --depth=1 https://github.com/gpakosz/.tmux.git ${HOME}/.tmux
 ln -s -f ${HOME}/.tmux/.tmux.conf ${HOME}
 
 step "clean up"
