8f0e008793b5966cf90af4db4aacf0f4c1b20c8d
Tzu-Yee Yang <fdja1974@gmail.com>
2021-05-29 13:16:18 +0800

    Add files via upload

---
+++ windows_install.ps1
@@ -0,0 +1,25 @@
+# $ErrorActionPreference = 'SilentlyContinue'
+
+# iwr -useb get.scoop.sh | iex # Download scoop
+
+scoop bucket add extras
+
+scoop install sudo
+
+sudo scoop install 7zip git openssh --global
+
+scoop install aria2 curl grep sed less netcat micro touch bat fzf ripgrep fd zoxide
+
+# Initiate fd
+Invoke-Expression (& {
+    $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
+    (zoxide init --hook $hook powershell) -join "`n"
+})
+
+
+# Initiate zoxide
+Invoke-Expression (& {
+    $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
+    (zoxide init --hook $hook powershell) -join "`n"
+})
+
