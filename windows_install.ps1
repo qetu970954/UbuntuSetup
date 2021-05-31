f3caaf37d773769ff200f6653cfdc456c6334d61
Tzu-Yee Yang <fdja1974@gmail.com>
2021-05-31 18:28:46 +0800

    Update windows_install.ps1

--- windows_install.ps1
+++ windows_install.ps1
@@ -10,12 +10,6 @@ sudo scoop install 7zip git openssh --global
 
 scoop install aria2 curl grep sed less netcat micro touch bat fzf ripgrep fd zoxide delta
 
-# Initiate fd
-Invoke-Expression (& {
-    $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
-    (zoxide init --hook $hook powershell) -join "`n"
-})
-
 
 # Initiate zoxide
 Invoke-Expression (& {
