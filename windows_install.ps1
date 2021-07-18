7f570c3723d8be37a2dbd455db5e4a9bda1bde58
Tzu-Yee Yang <fdja1974@gmail.com>
2021-07-18 20:58:03 +0800

    Update windows_install.ps1

--- windows_install.ps1
+++ windows_install.ps1
@@ -8,8 +8,10 @@ scoop install sudo
 
 sudo scoop install 7zip git openssh --global
 
-scoop install aria2 curl grep sed less netcat micro touch bat fzf ripgrep fd zoxide delta
+scoop install aria2 curl grep sed less netcat micro touch bat fzf ripgrep fd zoxide delta nu
 
+# nu = nushell, see 'https://github.com/nushell/nushell/'
+# Use this on windows to replace ugly powershell cmds
 
 # Initiate zoxide
 Invoke-Expression (& {
