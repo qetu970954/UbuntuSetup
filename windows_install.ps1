5aab5fb9ea1d2e7bf0216766ad11e424e423d302
Tzu-Yee Yang <fdja1974@gmail.com>
2021-08-29 21:42:34 +0800

    Update windows_install.ps1

--- windows_install.ps1
+++ windows_install.ps1
@@ -14,13 +14,12 @@ scoop install aria2
 scoop install curl grep sed less netcat micro touch bat fzf ripgrep fd zoxide delta
 scoop python@3.8.10
 
-
-
-
-# IMPORTANT!!!
-# Add this to your configuration (find it by running echo $profile in PowerShell):
+# to make zoxide work on powershell start
+@'
 Invoke-Expression (& {
     $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
     (zoxide init --hook $hook powershell) -join "`n"
 })
+'@ | Add-Content -Path (echo $profile)
+
 
